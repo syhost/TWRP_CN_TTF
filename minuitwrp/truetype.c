3ddb7aca070286912719c1aa7f9ee4281feca65f
syhost <syhost@163.com>
2014-11-29 21:30:46 +0800

    improve utf8_to_unicode function to support multi languages
    Change-Id: I62a34b163f00fc15467d28b82c95ba40e10567e5

--- minuitwrp/truetype.c
+++ minuitwrp/truetype.c
@@ -86,15 +86,40 @@ static FontData font_data = {
 static const uint32_t FNV_prime = 16777619U;
 static const uint32_t offset_basis = 2166136261U;
 
-int utf8_to_unicode(unsigned c1, unsigned c2, unsigned c3)
+int utf8_to_unicode(unsigned char* pIn, unsigned int *pOut)
 {
-	unsigned short unicode;
-	
-	unicode = (c1 & 0x1F) << 12;
-	unicode |= (c2 & 0x3F) << 6;
-	unicode |= (c3 & 0x3F);
-	
-	return unicode;
+    if(pIn == NULL || pOut == NULL)
+        return 0;
+
+    int utf_bytes = 1;
+    unsigned int unicode = 0;
+    unsigned char tmp;
+    tmp = *pIn++;
+    if (tmp < 0x80)
+    {
+        *pOut = tmp;
+    }
+    else
+    {
+        unsigned int high_bit_mask = 0x3F;
+        unsigned int high_bit_shift = 0;
+        int total_bits = 0;
+        while((tmp & 0xC0) == 0xC0)
+        {
+            utf_bytes ++;
+            if(utf_bytes > 6) return 0;
+            tmp = 0xFF & (tmp << 1);
+            total_bits += 6;
+            high_bit_mask >>= 1;
+            high_bit_shift++;
+            unicode <<= 6;
+            unicode |= (*pIn++) & 0x3F;
+        }
+        unicode |= ((tmp >> high_bit_shift) & high_bit_mask) << total_bits;
+        *pOut = unicode;
+    }
+
+    return utf_bytes;
 }
 
 static uint32_t fnv_hash(void *data, uint32_t len)
@@ -358,34 +383,21 @@ static int gr_ttf_render_text(TrueTypeFont *font, GGLSurface *surface, const cha
     TrueTypeFont *f = font;
     TrueTypeCacheEntry *ent;
     int max_len = 0, total_w = 0;
-    unsigned char c, c2, c3;
+    unsigned char c, utf_bytes = 0;
+    unsigned int unicode = 0;
     int i, x, diff, char_idx, prev_idx = 0;
     int height, base;
     FT_Vector delta;
     uint8_t *data = NULL;
     const char *text_itr = text;
     int *char_idxs;
-	
-	char_idxs = (int*)malloc(strlen(text) * sizeof(int));
-    while((c = *text_itr++))
+
+    char_idxs = (int*)malloc(strlen(text) * sizeof(int));
+    while((c = *text_itr))
     {
-		if (c < 0x80)
-		{
-			char_idx = FT_Get_Char_Index(f->face, c);
-		}
-		else
-		{
-			if ((c & 0xF0) == 0xE0)
-			{
-				c2 = *text_itr++;
-				c3 = *text_itr++;
-				char_idx = FT_Get_Char_Index(f->face, utf8_to_unicode(c, c2, c3));
-			}
-			else
-			{
-				char_idx = FT_Get_Char_Index(f->face, c);
-			}
-		}
+        utf_bytes = utf8_to_unicode(text_itr, &unicode);
+        text_itr += (utf_bytes == 0)? 1: utf_bytes;
+        char_idx = FT_Get_Char_Index(f->face, unicode);
         char_idxs[max_len] = char_idx;
         ent = gr_ttf_glyph_cache_get(f, char_idx);
         if(ent)
@@ -410,11 +422,11 @@ static int gr_ttf_render_text(TrueTypeFont *font, GGLSurface *surface, const cha
     if(font->max_height == -1)
         gr_ttf_getMaxFontHeight(font);
 
-    if(font->max_height == -1) 
+    if(font->max_height == -1)
     {
-		free(char_idxs);
+        free(char_idxs);
         return -1;
-	}
+    }
 
     height = font->max_height;
 
@@ -449,7 +461,7 @@ static int gr_ttf_render_text(TrueTypeFont *font, GGLSurface *surface, const cha
         prev_idx = char_idx;
     }
 
-	free(char_idxs);
+    free(char_idxs);
     return max_len;
 }
 
@@ -559,7 +571,8 @@ int gr_ttf_maxExW(const char *s, void *font, int max_width)
     TrueTypeFont *f = font;
     TrueTypeCacheEntry *ent;
     int max_len = 0, total_w = 0;
-    unsigned char c, c2, c3;
+    unsigned char c, utf_bytes = 0;
+    unsigned int unicode;
     int char_idx, prev_idx = 0;
     FT_Vector delta;
     StringCacheEntry *e;
@@ -574,25 +587,10 @@ int gr_ttf_maxExW(const char *s, void *font, int max_width)
         return max_len;
     }
 
-    for(; (c = *s++); ++max_len)
+    for(; (c = *s); ++max_len)
     {
-		if (c < 0x80)
-		{
-			char_idx = FT_Get_Char_Index(f->face, c);
-		}
-		else
-		{
-			if ((c & 0xF0) == 0xE0)
-			{
-				c2 = *s++;
-				c3 = *s++;
-				char_idx = FT_Get_Char_Index(f->face, utf8_to_unicode(c, c2, c3));
-			}
-			else
-			{
-				char_idx = FT_Get_Char_Index(f->face, c);
-			}
-		}
+        utf_bytes = utf8_to_unicode(s, &unicode);
+        s += (utf_bytes == 0)?  1: utf_bytes;
         if(FT_HAS_KERNING(f->face) && prev_idx && char_idx)
         {
             FT_Get_Kerning(f->face, prev_idx, char_idx, FT_KERNING_DEFAULT, &delta);
